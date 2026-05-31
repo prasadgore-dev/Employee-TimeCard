3881a6587168ec198d19e06061e6477fa398931c
Prasad Gore <goreprasad21@gmail.com>
2026-05-31 19:12:37 +0530

    added production frontend URL to origin

--- Employee-Backend/src/server.ts
+++ Employee-Backend/src/server.ts
@@ -18,7 +18,7 @@ const app: Express = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: ['http://localhost:5174', 'http://localhost:5173', 'https://employee-timecard-1.onrender.com/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
